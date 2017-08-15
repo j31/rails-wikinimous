b301569c0a18611f8d041375698e199554213a1a
John Fish <j31@users.noreply.github.com>
2017-08-15 14:49:40 +0200

    basic CRUD and format

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+    @article.save
+  end
+
+  def create
+    @article = Article.create(params[:id])
+    @article.create(article_params)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
